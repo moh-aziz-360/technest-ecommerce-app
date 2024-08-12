760df223abdc167a0bde7be3d9deaf41c09a5f2c
Sophont-360 <abdulaziznexxl@gmai.com>
2024-08-12 06:50:24 +0000

    changed dir

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import productList from "../../../src/ProductList.json";
+import productList from "/src/ProductList.json";
 import "./ProductList.css";
 
 const Products = () => {
